0a4c2b136b38dc3cd4a5b4bdd3bc87f255196cc0
Keith Hamm <hkhamm@gmail.com>
2022-04-22 13:54:41 -0700

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,27 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { styled } from "@mui/material"
+import React from "react"
+import "./App.css"
 
-function App() {
-  const [count, setCount] = useState(0)
+const Container = styled("div")(() => ({
+  textAlign: "center",
+}))
 
+const Content = styled("div")(() => ({
+  backgroundColor: "#282c34",
+  minHeight: "100vh",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  fontSize: "calc(10px + 2vmin)",
+  color: "white",
+}))
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <Container>
+      <Content></Content>
+    </Container>
   )
 }
 
